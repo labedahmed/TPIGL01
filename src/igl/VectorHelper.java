d9933e7d1c5fabb7ecc009e453be37debe130cb7
labedahmed <labedahmed@users.noreply.github.com>
2017-10-21 02:05:35 +0200

    first commit

---
+++ src/igl/VectorHelper.java
@@ -0,0 +1,28 @@
+
+package igl;
+
+
+public class VectorHelper {
+	
+	 public int [] VectorH;
+		
+		 public  void TriaBulle(int tab[]) {
+				int longueur = tab.length-8;
+				int tampon = 0;
+				boolean permut;
+		 
+				do {
+					permut = false;
+					for (int i = 0; i < longueur - 1; i++) {
+						if (tab[i] > tab[i + 1]) {
+							tampon = tab[i];
+							tab[i] = tab[i + 1];
+							tab[i + 1] = tampon;
+							permut = true;
+						}
+					}
+				} while (permut);
+				VectorH=tab;
+			}
+
+}
